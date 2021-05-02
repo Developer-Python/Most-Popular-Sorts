e1b58c60a4ae0b52599a11654d85bbfe20f7d796
Евгений Орлов <60120830+Developer-Python@users.noreply.github.com>
2021-05-02 15:30:42 +0300

    Update sorts.py

--- sorts.py
+++ sorts.py
@@ -10,8 +10,10 @@ import time
 import random
 # Модуль для импортирования таблицы ASCII
 import string
+# Модуль для использования многопоточности
+import threading
 
-
+import asyncio
 
 #------------------------------------------------------------------------------#
 #------------------------  Глобальные переменные  -----------------------------#
@@ -19,11 +21,19 @@ import string
 
 
 
+<<<<<<< Updated upstream
 # [числа] Динамический список от 0 до 50000
 array_dynamic_num = [random.random() * 5000 for i in range(5000)]
 
 # [строки] Динамический список от 0 до 3500
 array_dynamic_str = list(' '.join(random.choice(string.ascii_lowercase) for i in range(3500)))
+=======
+# [числа] Динамический список от 0 до 3000
+array_dynamic_num = [random.random() * 3000 for i in range(3000)]
+
+# [строки] Динамический список от 0 до 10000
+array_dynamic_str = list(' '.join(random.choice(string.ascii_lowercase) for i in range(3000)))
+>>>>>>> Stashed changes
 
 
 
@@ -49,8 +59,7 @@ def decorator_testing_speed(sort):
 
         # Точка подсчёта итоговой скорости исполнения
         total_speed = time.time() - start_time
-
-        return f'{name_sort} - завершилась за: {str(total_speed)[:8]} \n'
+        print(f'{name_sort} - завершилась за: {str(total_speed)[:8]} \n')
 
     return speed_measurement
 
@@ -132,8 +141,14 @@ class Sorts():
 #-------------------------------  Запуск  -------------------------------------#
 #------------------------------------------------------------------------------#
 
+<<<<<<< Updated upstream
 START = Sorts.bubble_sort(array_dynamic_num)
 print(START)
 
 
 # Автор: Орлов Евгений
+=======
+# [Test speed - numbers]
+START = asyncio.run(Sorts.bubble_sort(array_dynamic_num))
+START_02 = asyncio.run(Sorts.bubble_sort(array_dynamic_num))
+>>>>>>> Stashed changes
